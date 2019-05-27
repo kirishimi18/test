e2d04e733119a22a12f0339b410992d7325a67b2
Kirishimi18 <adi.yerzhanova@gmail.com>
2019-05-28 01:20:58 +0600

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+
+// var number = 5;
+// var string = "Hello!";
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
+
